20373daa7b93c6af0908bdf66189d1b7f1e526e5
Lee Hour <lhour999@gmail.com>
2023-03-24 23:09:06 +0700

    added mock data and related compennents

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
 // import header data
-import { headerData } from "../data";
+import { headerData } from "../data/data";
 // import components
 import Nav from "./Nav";
 import NavMobile from "./NavMobile";
-import Socials from "./Socials";
+import Socials from "./Social";
 // import icons
 import { TiThMenuOutline } from "react-icons/ti";
 
@@ -12,7 +12,7 @@ export const Header = () => {
   const { logo } = headerData;
   const [isActive, setIsActive] = useState(false);
   const [navMobile, setNavMobile] = useState(false);
-  
+
   useEffect(() => {
     window.addEventListener("scroll", () => {
       window.scrollY > 50 ? setIsActive(true) : setIsActive(false);
